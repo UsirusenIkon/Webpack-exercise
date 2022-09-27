50447d0ab8d1e0c42e95b536d5cd4f188d62ee98
Usirusen Ikon <lexicon048@gmail.com>
2022-09-27 12:30:37 +0100

    Run npm build command

--- dist/print.bundle.js
+++ dist/print.bundle.js
@@ -1,13 +1,4 @@
-/*
- * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
- * This devtool is neither made for production nor for readable output files.
- * It uses "eval()" calls to create a separate source file in the browser devtools.
- * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
- * or disable the default devtool with "devtool: false".
- * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
- */
-/******/ (() => { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ({
+(self["webpackChunkwebpack_exercise"] = self["webpackChunkwebpack_exercise"] || []).push([["print"],{
 
 /***/ "./src/print.js":
 /*!**********************!*\
@@ -15,18 +6,13 @@
   \**********************/
 /***/ (() => {
 
-eval("\n\n//# sourceURL=webpack://webpack-exercise/./src/print.js?");
+
 
 /***/ })
 
-/******/ 	});
-/************************************************************************/
-/******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/print.js"]();
-/******/ 	
-/******/ })()
-;
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/print.js"));
+/******/ }
+]);
